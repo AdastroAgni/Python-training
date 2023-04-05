bb37c195ab1d912512f117d8ae7602f404044268
AdastroAgni <kurs.astr.grain@inbox.ru>
2023-04-05 23:54:22 +0300

    add first commit of Lesson

---
+++ first.py
@@ -0,0 +1,17 @@
+print("Hello again man!")
+
+
+manInput = input("Enter your car: ")
+
+arrayOfcar = ["model_1", "model_2", "model_3"]
+
+if manInput == arrayOfcar[0]:
+    myCarIs = "model_1"
+elif manInput == arrayOfcar[1]:
+    myCarIs = "model_2"
+elif manInput == arrayOfcar[2]:
+    myCarIs = "model_3"
+
+print(f"I love this car: {myCarIs}")
+
+
